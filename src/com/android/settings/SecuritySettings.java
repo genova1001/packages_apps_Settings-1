0f8f52080adadfafd57186532b3ea6c47309cf27
maxwen <max.weninger@gmail.com>
2015-08-03 00:40:58 +0200

    [1/2] Settings: keyguard wallpaper config
    Squashed commit:
    [2/2] OmniGears: lockscreen wallpaper config
    Author: maxwen
    https://github.com/omnirom/android_packages_apps_OmniGears/commit/cd7421a28104e10990b9fe46d114b76ad4bf6b1a
    
    Adapted for Marshmallow
    
    Change-Id: I7d4e0162555ae53b3098727c467a8436ddeaee07

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -222,6 +222,10 @@ public class SecuritySettings extends SettingsPreferenceFragment
             });
         }
 
+        if (!mLockPatternUtils.isLockScreenDisabled(MY_USER_ID)) {
+            addPreferencesFromResource(R.xml.security_settings_more);
+        }
+
         if (mIsPrimary) {
             if (LockPatternUtils.isDeviceEncryptionEnabled()) {
                 // The device is currently encrypted.
