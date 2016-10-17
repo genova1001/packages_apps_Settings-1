sooorajjj/android_packages_apps_Settings

AppOps: Add more operations in app ops settings.
Add following operations in app ops settings.

-- Wifi enable/disable
-- Bluetooth enable/disable
-- Mobile data enable/disable

Change-Id: I32183d77b88c326168557444a445765b5a69de6f

src/com/android/settings/applications/AppOpsState.java
@@ -27,9 +27,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.text.format.DateUtils;
-
 import android.util.Log;
 import android.util.SparseArray;
+
 import com.android.settings.R;
 
 import java.io.File;
@@ -158,9 +158,15 @@ public class AppOpsState {
                     AppOpsManager.OP_SYSTEM_ALERT_WINDOW,
                     AppOpsManager.OP_CAMERA,
                     AppOpsManager.OP_RECORD_AUDIO,
-                    AppOpsManager.OP_PLAY_AUDIO },
+                    AppOpsManager.OP_PLAY_AUDIO,
+                    AppOpsManager.OP_WIFI_CHANGE,
+                    AppOpsManager.OP_BLUETOOTH_CHANGE,
+                    AppOpsManager.OP_DATA_CONNECT_CHANGE },
             new boolean[] { false,
                     false,
+                    true,
+                    true,
+                    true,
                     true,
                     true,
                     true,

genova1001/packages_apps_Settings-1

src/com/android/settings/applications/AppOpsState.java
@@ -194,7 +194,9 @@ public class AppOpsState {
                     AppOpsManager.OP_PROJECT_MEDIA,
                     AppOpsManager.OP_ACTIVATE_VPN,
                     AppOpsManager.OP_ASSIST_STRUCTURE,
-                    AppOpsManager.OP_ASSIST_SCREENSHOT},
+                    AppOpsManager.OP_ASSIST_SCREENSHOT,
+                    AppOpsManager.OP_WIFI_CHANGE,
+                    AppOpsManager.OP_BLUETOOTH_CHANGE},
             new boolean[] { false,
                     true,
                     true,
@@ -204,7 +206,9 @@ public class AppOpsState {
                     false,
                     false,
                     false,
-                    false }
+                    false,
+                    true,
+                    true }
             );
 
     public static final OpsTemplate[] ALL_TEMPLATES = new OpsTemplate[] {